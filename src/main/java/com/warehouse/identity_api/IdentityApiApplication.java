1c44fd3be5c65e4c40afcfb75b71c6284043904d
MSI\Phuong Anh <ltpa1203@gmail.com>
2025-05-15 18:29:27 +0700

    first commit

---
+++ src/main/java/com/warehouse/identity_api/IdentityApiApplication.java
@@ -0,0 +1,13 @@
+package com.warehouse.identity_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityApiApplication.class, args);
+	}
+
+}
